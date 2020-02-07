a83fc8ca3a9798a3768dc9e0cfc95ccb39feb4e5
Connie Liu <liu.connie.cs@gmail.com>
2020-02-06 19:15:39 -0500

    Terms and Conditiions link to terms page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import {Container} from "@material-ui/core";
-import Link from "@material-ui/core/Link";
+import {HashLink as Link} from 'react-router-hash-link';
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+  link: {
+    margin: theme.spacing(2, 2.5),
+    textDecoration: "none",
+    color: "inherit",
+    fontWeight: "500"
+  },
+}));
 
 export default function Footer() {
+  const classes = useStyles();
   return (
     <AppBar color="default" style={{top: "auto", bottom: 0, position: "relative", textAlign: "center", padding: "1em 0"}}>
       <Container maxWidth="sm">
         <div>Copyright © 24Vita LLC 2020.</div>
         <div>111 Monroe St #2C Brooklyn NY, 11216</div>
-        <Link component="div">Terms and Conditions</Link>
+        <Link variant="button" to="/term" className={classes.link}>
+          Terms & Conditions
+        </Link>
       </Container>
     </AppBar>
   );
