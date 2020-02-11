ffdda0e701cff6332508002184e02a2c376d4cdf
Connie Liu <liu.connie.cs@gmail.com>
2020-02-10 22:08:54 -0500

    Edit background image and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,14 +12,14 @@ export default function Header() {
     <div style={{paddingTop: '4em'}}>
       <Parallax
         bgImage={backgroundImage}
-        strength={400}
+        strength={100}
       >
-        <div style={{height: '45em'}}>
+        <div className="header-div">
           <Container maxWidth="sm" color="default" component="main" className="header-container">
-            <Typography style={{fontWeight: '300'}} variant="h2" align="center" color="inherit" gutterBottom>
+            <Typography style={{color: "#FFF", fontSize: '3em'}} variant="p" component="p" align="left" gutterBottom>
               Your virtual health relationship manager is here
             </Typography>
-            <Typography style={{fontWeight: '500'}} variant="h5" align="center" color="inherit" component="p">
+            <Typography style={{color: "#FFF", fontSize: '1.5em'}} variant="p" align="left" component="p">
               Our reliable Health Relationship Managers (HRMs) help providers document comprehensive notes in real-time
               and
               coordinate patient activities between clinicians, departments and healthcare organizations. All in one.
